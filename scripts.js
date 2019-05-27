1cfd68abd213e89ecae129a423f406f12323bf94
SohamSarfare <sohamsarfare@gmail.com>
2019-05-27 12:12:30 +0530

    Added carousel

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshowslide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
+
+function nextSlide(index){
+	var slides = document.getElementsByClassName("slideshowslide");
+	slides[index].style.display = "block";
+	if(index == 0){
+		slides[1].style.display = "none";
+	}
+	else{
+		slides[0].style.display = "none";
+	}
+	//showSlides();
+}
